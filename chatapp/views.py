3ef9d293ab770aa08862f53601b79d204485af15
hamuham <uni.uni.yui@hotmail.co.jp>
2023-06-06 17:57:52 +0900

    createUser

--- chatapp/views.py
+++ chatapp/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
 
 # Create your views here.
 
 def signupfunc(request):
+    # ユーザー作成処理
+    print(request)
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = User.objects.create_user(username, 'test@test.com', password)
     return render(request, 'singup.html', {'some':100})
