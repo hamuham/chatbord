800ab24cf9fc5893bd0c531de0a4492fb443abc7
hamuham <uni.uni.yui@hotmail.co.jp>
2023-06-08 08:40:21 +0900

    createUserIntegrityError

--- chatapp/views.py
+++ chatapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
+from django.db import IntegrityError
 
 # Create your views here.
 
@@ -8,6 +9,12 @@ def signupfunc(request):
     print(request)
     if request.method == "POST":
         username = request.POST['username']
+        email = request.POST['email']
         password = request.POST['password']
-        user = User.objects.create_user(username, 'test@test.com', password)
+        try:
+            user = User.objects.create_user(username, email, password)
+        except IntegrityError:
+            return render(request, 'singup.html', {'error':'このユーザーは登録されています'})
+
+
     return render(request, 'singup.html', {'some':100})
