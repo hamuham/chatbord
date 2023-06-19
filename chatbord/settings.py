f6f37219e2ebd689f61d4d851329e75e5d9135ce
hamuham <uni.uni.yui@hotmail.co.jp>
2023-06-19 21:56:04 +0900

    bootstrap.static

--- chatbord/settings.py
+++ chatbord/settings.py
@@ -110,12 +110,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = '/static/'
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+STATICFILES_DIRS = [str(BASE_DIR / 'static')]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+# DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+MEDIA_URL = 'mediamedia/'
 
 MEDIA_ROOT = BASE_DIR / 'media'
-MEDIA_URL = 'medi/'
