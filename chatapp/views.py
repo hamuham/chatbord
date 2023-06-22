7fbb0cf3f76023cdf3bc9280211ca78c5a17616d
hamuham <uni.uni.yui@hotmail.co.jp>
2023-06-22 22:41:58 +0900

    detail,404

--- chatapp/views.py
+++ chatapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.models import User
 from django.db import IntegrityError
 from django.contrib.auth import authenticate, login, logout
@@ -43,3 +43,7 @@ def listfunc(request):
 def logoutfunc(request):
     logout(request)
     return redirect('login')
+
+def detailfunc(request, pk):
+    object = get_object_or_404(ChatModel, pk=pk)
+    return render(request, 'detail.html', {'object':object})
